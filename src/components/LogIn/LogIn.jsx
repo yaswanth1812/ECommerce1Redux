79a72a3fbb8e713b879a6a42014a44f84624124e
yaswanth1812 <yaswanthkadimi519@gmail.com>
2024-04-15 23:39:30 +0530

    added

--- src/components/LogIn/LogIn.jsx
+++ src/components/LogIn/LogIn.jsx
@@ -9,7 +9,9 @@ import {
     Button,
   } from "@material-tailwind/react";
   import {login} from '../../features/slices/authSlice';
-  import {useDispatch} from "react-redux"
+  import {useDispatch} from "react-redux";
+  // import {useHistory} from "react-router-dom"
+
   
 const LogIn = () => {
     const initialState ={
@@ -23,6 +25,22 @@ const LogIn = () => {
         setValues({...values,[name]:value});
     };
     const dispatch = useDispatch();
+    // const history = useHistory();
+      const handleSignIn = ()=>{
+       
+  
+        const {name,password,image} = values;
+
+        const specialChar =  /[!@#$%^&*(),.?":{}|<>]/;
+        const capital = /[A-z]/;
+        const  numberReg = /[0-9]/;
+
+        if(!specialChar.test(password) || !capital.test(password) || ! numberReg.test(password)){
+          alert("Password must contain at least one special character, one capital letter, and one number")
+          return; 
+        }
+        dispatch(login(values));
+      }
   return (
     <div className="grid grid-cols-1 items-center justify-items-center h-screen" style={{backgroundImage:'url(https://wallpaperbat.com/img/759579-cybersecurity-wallpaper.png)'}}>
       <Card className="w-96">
@@ -63,13 +81,22 @@ const LogIn = () => {
         <Button 
         variant="gradient" 
         fullWidth
-         onClick={()=> dispatch(login(values))}> 
+         onClick={handleSignIn}> 
           Sign In
         </Button>
         <Typography variant="small" className="mt-6 flex justify-center">
       Image is Optional
         </Typography>
       </CardFooter>
+      {/* <CardFooter className='pt-1'>
+          <Button
+            variant='gradient'
+            fullWidth
+            onClick={handleSkip}>
+            Skip
+          </Button>
+        </CardFooter> */}
+
     </Card>
     </div>
   )
